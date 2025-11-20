7a4f88b77dab57e6aa02fcac38a244eedb2131df
Sujan Budhathoki <sujanbudhathoki123@gmail.com>
2025-11-20 19:40:39 +1100

    Add zod schema and types for order

--- lib/validators.ts
+++ lib/validators.ts
@@ -80,7 +80,28 @@ export const paymentMethodSchema = z
   .object({
     type: z.string().min(1, 'Payment method is required'),
   })
-  .refine(data => PAYMENT_METHODS.includes(data.type),{
-    path:['type'],
-    message:'Invalid payment method'
+  .refine(data => PAYMENT_METHODS.includes(data.type), {
+    path: ['type'],
+    message: 'Invalid payment method',
   });
+
+export const insertOrderSchema = z.object({
+  userId: z.string().min(1, 'User is required'),
+  itemsPrice: currency,
+  totalPrice: currency,
+  shippingPrice: currency,
+  taxPrice: currency,
+  paymentMethod: z.string().refine(data => PAYMENT_METHODS.includes(data), {
+    message: 'Invalid payment method',
+  }),
+  shippingAddress: shippingAddressSchema,
+});
+
+export const insertOrderItemSchema = z.object({
+  productId: z.string(),
+  slug: z.string(),
+  image: z.string(),
+  name: z.string(),
+  price: currency,
+  qty:z.number()
+});
